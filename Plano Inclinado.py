f1c87b38dc1a5b5eb882ff4616e9a4ab2cca5f34
Edwin Laverde <78189732+edwin9532@users.noreply.github.com>
2021-05-17 13:06:26 -0500

    Update Plano Inclinado.py

--- Plano Inclinado.py
+++ Plano Inclinado.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, pymunk
 
 pygame.init()
 screen,running = pygame.display.set_mode((800,600)),True
@@ -7,8 +7,31 @@ ic = pygame.image.load("Imagenes/1.png")
 pygame.display.set_icon(ic)
 pygame.display.flip()
 
+def bola(space):
+    body = pymunk.Body(10,20,body_type= pymunk.Body.DYNAMIC)
+    body.position = (100,100)
+    shape = pymunk.Circle(body, 10)
+    space.add(body,shape)
+    return shape
+
+def draw_bola(bola):
+    pos_x = int(bola.body.position.x)
+    pos_y = int(bola.body.position.y)
+    pygame.draw.circle(screen, (255,255,255), (pos_x,pos_y), 5)
+
+clock = pygame.time.Clock()
+space = pymunk.Space()
+space.gravity = (0,100)
+fps = 50
+
+bola = bola(space)
+
 while running:
-    for e in pygame.event.get():
-        if e.type == pygame.QUIT: running = False
-        elif e.type == pygame.KEYDOWN:
-            if e.key == pygame.K_ESCAPE: running = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE: running = False
+    clock.tick(fps)
+    draw_bola(bola)
+    space.step(1/fps)
+    pygame.display.update()
