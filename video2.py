aaeba9ae86846dadc67c1a90bf33208bf1459b5f
Liz <lgonzalezva@unal.edu.co>
2021-08-18 18:07:37 -0500

    updates

--- video2.py
+++ video2.py
@@ -6,3 +6,27 @@ Created on Wed Aug 18 17:16:34 2021
 @author: lizeth
 """
 
+import  cv2
+
+def video2():
+    cap=cv2.VideoCapture('video2.mp4')
+    
+    
+    if (cap.isOpened()== False):
+        print("error ")
+    while (cap.isOpened()):
+        ret, frame=cap.read()
+        if ret == True:
+            cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
+            cv2.setWindowProperty("window",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+            cv2.imshow('frame', frame)
+            
+            if cv2.waitKey(25)==ord('q'):
+                break
+        else:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+           
+#video2()
