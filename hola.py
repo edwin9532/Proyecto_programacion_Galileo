af6b28c1bfe68215a91f0505fa6aca3aa365eb62
rubendario2021 <83649123+rubendario2021@users.noreply.github.com>
2021-08-18 09:31:48 -0500

    Add files via upload

---
+++ hola.py
@@ -0,0 +1,25 @@
+
+
+import  cv2
+
+def video():
+    cap=cv2.VideoCapture('Black Hole - 10.mp4')
+    
+    
+    if (cap.isOpened()== False):
+        print("error ")
+    while (cap.isOpened()):
+        ret, frame=cap.read()
+        if ret == True:
+            cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
+            cv2.setWindowProperty("window",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+            cv2.imshow('frame', frame)
+            
+            if cv2.waitKey(25)==ord('q'):
+                break
+        else:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+video()            
+    
